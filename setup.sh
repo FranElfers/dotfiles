f27f6e4589c482e55991ac6d997b248d1f888ba3
franelfers <fransozzan@gmail.com>
2026-08-22 19:36:55 -0300

    cliamp config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 pacman -Syu
-pacman -S nano yazi github-cli
+pacman -Sy nano yazi github-cli zed
 
 # omarchy plugins
 omarchy plugin add https://github.com/crmne/omarchy-hyprmoncfg.git --enable -y
@@ -7,8 +7,14 @@ omarchy plugin add https://github.com/crmne/omarchy-hyprmoncfg.git --enable -y
 # yazi config
 install -D yazi.toml ~/.config/yazi/yazi.toml
 
+# cliamp config
+install -D cliamp.toml ~/.config/cliamp/config.toml
+
 # github auth
 # gh auth login -p https -h github.com -w
 
 # hyprmod
 # yay -S hyprmod
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.7/install.sh | bash

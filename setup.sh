35d7510a2ba9883bc6c04455f10e81073e40f52b
franelfers <fransozzan@gmail.com>
2026-08-22 12:56:46 -0300

    hyprmod setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 pacman -Syu
 pacman -S nano yazi github-cli
 
+# omarchy plugins
+omarchy plugin add https://github.com/crmne/omarchy-hyprmoncfg.git --enable -y
+
 # yazi config
 install -D yazi.toml ~/.config/yazi/yazi.toml
 
 # github auth
 # gh auth login -p https -h github.com -w
+
+# hyprmod
+# yay -S hyprmod

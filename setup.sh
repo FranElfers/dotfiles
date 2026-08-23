cb220e57446598e325dde7cfab90307ae3597762
franelfers <fransozzan@gmail.com>
2026-08-23 00:53:38 -0300

    omarchy shell config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-pacman -Syu
-pacman -Sy nano yazi github-cli zed bun go
+omarchy update -y
+omarchy pkg add -y nano yazi github-cli zed bun go
 
 # omarchy plugins
 omarchy plugin add https://github.com/crmne/omarchy-hyprmoncfg.git --enable -y
@@ -10,6 +10,8 @@ install -D cliamp.toml ~/.config/cliamp/config.toml
 install -D hyprland-gui.lua ~/.config/hypr/hyprland-gui.lua
 install -D .bashrc ~/.bashrc
 install -D .bash_profile ~/.bash_profile
+install -D .omarchy_shell.json ~/.config/omarchy/shell.json
+install -D workspace/1.lua ~/.local/state/omarchy/workspace-layouts/1.lua 
 
 # github auth
 # gh auth login -p https -h github.com -w

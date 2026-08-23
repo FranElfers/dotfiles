287152cf4f32417f86b538a9d7d2dd495a204eb3
Francael <39459727+FranElfers@users.noreply.github.com>
2026-08-23 02:11:41 -0300

    USER_HOME for installations

--- setup.sh
+++ setup.sh
@@ -1,17 +1,22 @@
+#!/bin/bash
+
 omarchy update -y
-omarchy pkg add -y nano yazi github-cli zed bun go
+omarchy pkg add nano yazi github-cli zed bun go
 
 # omarchy plugins
 omarchy plugin add https://github.com/crmne/omarchy-hyprmoncfg.git --enable -y
 
 # configs
-install -D yazi.toml ~/.config/yazi/yazi.toml
-install -D cliamp.toml ~/.config/cliamp/config.toml
-install -D hyprland-gui.lua ~/.config/hypr/hyprland-gui.lua
-install -D .bashrc ~/.bashrc
-install -D .bash_profile ~/.bash_profile
-install -D .omarchy_shell.json ~/.config/omarchy/shell.json
-install -D workspace/1.lua ~/.local/state/omarchy/workspace-layouts/1.lua 
+USER_HOME="${HOME}"
+[ -n "$SUDO_USER" ] && USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+
+install -D yazi.toml "$USER_HOME/.config/yazi/yazi.toml"
+install -D cliamp.toml "$USER_HOME/.config/cliamp/config.toml"
+install -D hyprland-gui.lua "$USER_HOME/.config/hypr/hyprland-gui.lua"
+install -D .bashrc "$USER_HOME/.bashrc"
+install -D .bash_profile "$USER_HOME/.bash_profile"
+install -D omarchy_shell.json "$USER_HOME/.config/omarchy/shell.json"
+install -D workspace/1.lua "$USER_HOME/.local/state/omarchy/workspace-layouts/1.lua"
 
 # github auth
 # gh auth login -p https -h github.com -w

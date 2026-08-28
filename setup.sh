13b4d7b23ced2942972477bc62d2331afc399496
franelfers <fransozzan@gmail.com>
2026-08-28 02:12:33 -0300

    Squashed commit of the following:
    commit 0c1be874844f0707573c3f6b60b07196124ee178
    Author: franelfers <fransozzan@gmail.com>
    Date:   Fri Aug 28 02:12:18 2026 -0300
    
        migro installer a setup
    
    commit 0cefbd67d63c4f8fb8159c376817350829d00d92
    Author: franelfers <fransozzan@gmail.com>
    Date:   Fri Aug 28 02:11:16 2026 -0300
    
        Update installer.sh
    
    commit e538059143c5796ea3e2cf0560e9a42ae085bfe9
    Merge: db68cca be0f276
    Author: franelfers <fransozzan@gmail.com>
    Date:   Fri Aug 28 01:54:26 2026 -0300
    
        Merge branch 'master' into installer
    
    commit db68ccadb193d4323e20e94311f056879d1aa4aa
    Author: Francisco Elfers <fransozzan@gmail.com>
    Date:   Fri Aug 28 01:44:53 2026 -0300
    
        plugins en paralelo
    
    commit 4011b683a4436125e5056e8b856350cbef4b1ec6
    Author: Francisco Elfers <fransozzan@gmail.com>
    Date:   Fri Aug 28 01:41:53 2026 -0300
    
        full installation
    
    commit 276a267c3fd91bcb1e33d54637c61425bfdb19b9
    Author: Francisco Elfers <fransozzan@gmail.com>
    Date:   Fri Aug 28 01:34:40 2026 -0300
    
        fix 2
    
    commit f8f7825e35d446ce02fb1988e4dbb2f3dc973c9a
    Author: Francisco Elfers <fransozzan@gmail.com>
    Date:   Fri Aug 28 01:25:23 2026 -0300
    
        fix
    
    commit 887958433bad9d7c5ed61e87e23e415cf1a08c72
    Author: Francisco Elfers <fransozzan@gmail.com>
    Date:   Fri Aug 28 01:23:15 2026 -0300
    
        installer test

--- setup.sh
+++ setup.sh
@@ -1,79 +1,113 @@
 #!/bin/bash
+clear
 
-# correr esto antes de conectarse a internet si se instalo en una vm
-# sudo ufw allow in on virbr0
-# sudo ufw route allow in on virbr0
+trap 'tput csr 0 $(tput lines); tput cnorm; echo; exit' INT TERM EXIT
 
-omarchy update -y
-omarchy pkg add nano yazi github-cli zed bun go fuse2 webkit2gtk-4.1 uv syncthing flatpak
+tput civis
+tput csr 0 $(($(tput lines) - 2))
 
-# hyprmod, omazed
-yay -S hyprmod omazed --noconfirm
-omazed setup
-
-# omarchy plugins
-omarchy plugin add https://github.com/crmne/omarchy-hyprmoncfg.git --enable -y &
-omarchy plugin add https://github.com/bscott/cliamp-oma-plugin.git --enable -y &
-omarchy plugin add https://github.com/SirJul1337/omarchy-lock-explorer.git --enable -y &
-omarchy plugin add https://github.com/brianblakely/omarchy-plugins.git --enable -y &
-omarchy plugin add https://github.com/ussego/otoru.git --enable -y &
-omarchy plugin add https://github.com/TheTrueFerret/omarchy-decent-workspaces.git --enable -y &
-omarchy plugin add https://github.com/ssupt/omarchy-bluetooth-audio.git --enable -y &
-omarchy plugin add https://github.com/edgarsilva/omarchy-hw-monitor.git --enable -y &
-omarchy plugin add https://github.com/ssupt/omarchy-audio-control.git --enable -y &
-wait
-
-# configs
-USER_HOME="${HOME}"
-[ -n "$SUDO_USER" ] && USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
-
-REPO_RAW_URL="https://raw.githubusercontent.com/FranElfers/dotfiles/master"
-
-download_config() {
-    local dest="$USER_HOME/$1"
-    mkdir -p "$(dirname "$dest")"
-    curl -fSL "$REPO_RAW_URL/$1" -o "$dest"
-    [ -n "$SUDO_USER" ] && chown "$SUDO_USER:" "$dest"
+update_omarchy() {
+    omarchy update -y
 }
 
-download_config .bash_profile
-download_config .bashrc
-download_config .config/btop/btop.conf
-download_config .config/cliamp/config.toml
-download_config .config/cliamp/playlists/radio-stations.toml
-download_config .config/gtk-3.0/bookmarks
-download_config .config/hypr/hyprland-gui.lua
-download_config .config/hypr/hyprland.lua
-download_config .config/hypr/input.lua
-download_config .config/hypr/monitors.lua
-download_config .config/omarchy/audio-preferences.json
-download_config .config/omarchy/audio-rules.json
-download_config .config/omarchy/shell.json
-download_config .config/omarchy/ussego.otoru.json
-download_config .config/yazi/yazi.toml
-download_config .config/zed/settings.json
-download_config .gemini/antigravity-cli/settings.json
-download_config .local/state/omarchy/powerprofiles/battery
-download_config .local/state/omarchy/workspace-layouts/1.lua
-download_config .local/state/syncthing/config.xml
+install_packages_pacman() {
+    omarchy pkg add nano yazi github-cli zed bun go fuse2 webkit2gtk-4.1 uv syncthing flatpak
+}
 
-# nvm, pnpm, antigravity, antra
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.7/install.sh | bash &
-curl -fsSL https://get.pnpm.io/install.sh | sh - &
-curl -fsSL https://antigravity.google/cli/install.sh | bash
-curl -Lo "$USER_HOME/Antra.AppImage" https://github.com/anandprtp/Antra/releases/latest/download/Antra-Linux.AppImage &
-wait
-chmod +x "$USER_HOME/Antra.AppImage"
+install_packages_aur() {
+    yay -S hyprmod omazed --noconfirm
+    omazed setup
+}
 
-# github auth
-# gh auth login -p https -h github.com -w
+install_plugins() {
+    omarchy plugin add https://github.com/crmne/omarchy-hyprmoncfg.git --enable -y &
+    omarchy plugin add https://github.com/bscott/cliamp-oma-plugin.git --enable -y &
+    omarchy plugin add https://github.com/SirJul1337/omarchy-lock-explorer.git --enable -y &
+    omarchy plugin add https://github.com/brianblakely/omarchy-plugins.git --enable -y &
+    omarchy plugin add https://github.com/ussego/otoru.git --enable -y &
+    omarchy plugin add https://github.com/TheTrueFerret/omarchy-decent-workspaces.git --enable -y &
+    omarchy plugin add https://github.com/ssupt/omarchy-bluetooth-audio.git --enable -y &
+    omarchy plugin add https://github.com/edgarsilva/omarchy-hw-monitor.git --enable -y &
+    omarchy plugin add https://github.com/ssupt/omarchy-audio-control.git --enable -y &
+    wait
+}
 
-# set lock screen
-omarchy restart shell
+download_configs() {
+    USER_HOME="${HOME}"
+    [ -n "$SUDO_USER" ] && USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
 
-# swap fn & ctrl keys on macbook
-# echo "options hid_apple swap_fn_leftctrl=1" | sudo tee /etc/modprobe.d/hid_apple.conf
-# sudo mkinitcpio -P
+    REPO_RAW_URL="https://raw.githubusercontent.com/FranElfers/dotfiles/master"
 
-# fix macbook T2 speakers
-# curl -sSL https://raw.githubusercontent.com/ngodn/linux-t2-mbp16_1-arch-audio-setup/main/install.sh | bash
+    download_config() {
+        local dest="$USER_HOME/$1"
+        mkdir -p "$(dirname "$dest")"
+        curl -fSL "$REPO_RAW_URL/$1" -o "$dest"
+        [ -n "$SUDO_USER" ] && chown "$SUDO_USER:" "$dest"
+    }
+
+    download_config .bash_profile
+    download_config .bashrc
+    download_config .config/btop/btop.conf
+    download_config .config/cliamp/config.toml
+    download_config .config/cliamp/playlists/radio-stations.toml
+    download_config .config/gtk-3.0/bookmarks
+    download_config .config/hypr/hyprland-gui.lua
+    download_config .config/hypr/hyprland.lua
+    download_config .config/hypr/input.lua
+    download_config .config/hypr/monitors.lua
+    download_config .config/omarchy/audio-preferences.json
+    download_config .config/omarchy/audio-rules.json
+    download_config .config/omarchy/shell.json
+    download_config .config/omarchy/ussego.otoru.json
+    download_config .config/yazi/yazi.toml
+    download_config .config/zed/settings.json
+    download_config .gemini/antigravity-cli/settings.json
+    download_config .local/state/omarchy/powerprofiles/battery
+    download_config .local/state/omarchy/workspace-layouts/1.lua
+    download_config .local/state/syncthing/config.xml
+}
+
+download_external_apps() {
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.7/install.sh | bash &
+    curl -fsSL https://get.pnpm.io/install.sh | sh - &
+    curl -fsSL https://antigravity.google/cli/install.sh | bash
+    curl -Lo "$USER_HOME/Antra.AppImage" https://github.com/anandprtp/Antra/releases/latest/download/Antra-Linux.AppImage &
+    wait
+    chmod +x "$USER_HOME/Antra.AppImage"
+}
+
+end() {
+    omarchy restart shell
+}
+
+scripts=(
+    update_omarchy
+    install_packages_pacman
+    install_packages_aur
+    install_plugins
+    download_configs
+    download_external_apps
+    end
+)
+
+titles=(
+    "Actualizando Omarchy"
+    "Instalando paquetes (pacman)"
+    "Instalando paquetes (AUR)"
+    "Instalando plugins de Omarchy"
+    "Descargando configuraciones"
+    "Descargando Apps externas"
+    "Finalizando"
+)
+
+for (( i=0; i<${#scripts[@]}; i++ )); do
+    clear
+    tput sc
+    tput cup $(tput lines) 0
+    echo -ne " <=========[  ${titles[i]}  ]=========>"
+    tput rc
+    ${scripts[$i]}
+done
+
+clear
+echo -e "\n¡Instalación completada!"

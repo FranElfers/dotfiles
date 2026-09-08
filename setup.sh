063e1108640e7b396d53dd6d581fc6467a962a54
franelfers <fransozzan@gmail.com>
2026-09-08 01:04:22 -0300

    .config/hypr/bindings.lua

--- setup.sh
+++ setup.sh
@@ -73,6 +73,7 @@ download_configs() {
     download_config .config/foot/foot.ini
     download_config .config/gtk-3.0/bookmarks
     download_config .config/htop/htoprc
+    download_config .config/hypr/bindings.lua
     download_config .config/hypr/hyprland-gui.lua
     download_config .config/hypr/hyprland.lua
     download_config .config/hypr/input.lua
